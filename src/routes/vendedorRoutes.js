abd0e9b695101c8cbb0ff550bf235b6e8dd74d28
Diana Rebeca Ortega <diana.os@tecjerez>
2026-06-04 10:32:10 -0600

    Se genro la alta de cliente

--- src/routes/vendedorRoutes.js
+++ src/routes/vendedorRoutes.js
@@ -5,5 +5,7 @@ const vendedorController = require('../controllers/vendedorController');
 
 router.get('/vendedor', auth, vendedorController.mostrarVentas);
 router.get('/vendedor/clientes', auth, vendedorController.listarClientes);
+//router.get('/vendedor/clientes/editar/:id', auth, vendedorController.editarFormulario);
 router.get('/clientes', auth, vendedorController.listarClientes);
+router.post('/vendedor/clientes/crear', auth, vendedorController.crearCliente);
 module.exports = router;
