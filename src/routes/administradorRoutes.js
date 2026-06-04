0337774a3fdd8702d9a0f4cd46e354b16e828394
Diana Rebeca Ortega <diana.os@tecjerez>
2026-06-04 05:17:18 -0600

    Se redirecciono la Alta de Administrador para crear a vendedro de autos

--- src/routes/administradorRoutes.js
+++ src/routes/administradorRoutes.js
@@ -6,11 +6,11 @@ const administradorController = require('../controllers/administradorController'
 router.get('/administrador', administradorController.listarEmpleados);
 
 //---------------------ABCC de vendedor--------------------
-router.post('/crear', administradorController.crearEmpleado);// 2. Ruta para procesar el formulario de alta
-router.get('/detalle/:idVendedor', administradorController.verDetalle);// 3. Ruta para ver el detalle de un vendedor específico
-router.get('/editar/:idVendedor', administradorController.editarFormulario);// 4  Ruta para mostrar el formulario de edición
-router.post('/editar/:idVendedor', administradorController.actualizarEmpleado);//5 Ruta para guardar los cambios
-router.post('/eliminar/:idVendedor', administradorController.eliminarEmpleado);
+router.post('/administrador/crear', administradorController.crearEmpleado);// 2. Ruta para procesar el formulario de alta
+router.get('/administrador/detalle/:idVendedor', administradorController.verDetalle);// 3. Ruta para ver el detalle de un vendedor específico
+router.get('/administrador/editar/:idVendedor', administradorController.editarFormulario);// 4  Ruta para mostrar el formulario de edición
+router.post('/administrador/editar/:idVendedor', administradorController.actualizarEmpleado);//5 Ruta para guardar los cambios
+router.post('/administrador/eliminar/:idVendedor', administradorController.eliminarEmpleado);
 
 
 module.exports = router;
