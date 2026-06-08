419240d1320cf657ac83df8aefd197473e93e9e2
Diana Rebeca Ortega <diana.os@tecjerez>
2026-06-07 19:45:05 -0600

    Se añadio la vista para ver los detalles del carro

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -26,6 +26,23 @@ explorarAutos: async (req, res) => {
         console.error(error);
         res.status(500).send('Error al cargar catálogo');
     }
+},
+verDetalle: async (req, res) => {
+    try {
+        const { id } = req.params;
+        // Buscamos el auto por su idAutomovil
+        const [rows] = await db.query("SELECT * FROM automovil WHERE idAutomovil = ?", [id]);
+        
+        if (rows.length === 0) return res.status(404).send('Auto no encontrado');
+        
+        res.render('Vistas_Cliente/detalle_auto', {
+            usuario: req.session.usuario,
+            auto: rows[0] 
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al cargar detalle');
+    }
 }
 
 };
--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const clienteController = require('../controllers/clienteController');
+
 //clientes
 router.get('/cliente', auth, clienteController.mostrarCarros);
 router.get('/cliente/explorar_autos', auth, clienteController.explorarAutos);
+router.get('/cliente/detalle/:id', auth, clienteController.verDetalle);
+
+
+
 module.exports = router;
